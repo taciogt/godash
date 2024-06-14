b6a7ba8b0c226972019943bfc21584ff7d20d49f
Tácio Tavares <tacio@latitud.com>
2024-06-13 22:23:13 -0300

    Add 'Find' function in godash.go
    The 'Find' function has been introduced to the godash.go file. This function returns the first element in the provided slice that satisfies the specified predicate function. In case no element meets the predicate, it returns the zero value of the element type along with false.

--- godash.go
+++ godash.go
@@ -5,6 +5,8 @@ type Slice[T any] []T
 //type Slice interface {
 //}
 
+// Find returns the first element in the given slice that satisfies the provided predicate function.
+// If no element satisfies the predicate, the zero value of the element type is returned along with false.
 func Find[T any, S ~[]T](s S, predicate func(v T) bool) (T, bool) {
 	for _, v := range s {
 		if predicate(v) {
